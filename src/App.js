9a489843662367dcbf2f6b248e285b360bbfe4b8
mahesh <maheshmb7712@gmail.com>
2022-04-06 21:38:48 +0530

    context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React,{useEffect,useContext} from 'react';
 import './App.css';
 import { BrowserRouter as Router ,Route ,Switch } from 'react-router-dom';
 import Signup from './Components/Signup/Signup'
 import Login from './Components/Login/Login'
+import { AuthContext } from './store/Context';
+import {FirebaseContext} from './store/Context'
 /**
  * ?  =====Import Components=====
  */
 import Home from './Pages/Home';
 
 function App() {
+  const {setuser }=useContext(AuthContext)
+  const {firebase }=useContext(FirebaseContext )
+  useEffect(()=>{
+      firebase.auth().onAuthStateChanged((user=>{
+        setuser(user)
+      }))
+  }) 
   return (
+
     <div>
     <Router>
 
--- src/store/Context.js
+++ src/store/Context.js
@@ -2,13 +2,12 @@ import {createContext,useState} from 'react'
 export const FirebaseContext =createContext(null);
 
 
-export const AuthContext=createContext(null)
-
+export const AuthContext=createContext(null)  
 export default function Context({children})
  {
     const [user,setuser]=useState(null)
   return (
-    <AuthContext.Provider>
+    <AuthContext.Provider value={{user,setuser}}>
         {children}
     </AuthContext.Provider>
   )
