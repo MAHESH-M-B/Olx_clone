95a3a7f06a9fdc3aebcbc935fb44c4a2349e4f45
mahesh <maheshmb7712@gmail.com>
2022-04-23 21:51:57 +0530

    post component completed successfully

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,9 +1,24 @@
-import React from 'react';
-
+import React,{useState,useEffect,useContext} from 'react';
 import Heart from '../../assets/Heart';
 import './Post.css';
-
+import {FirebaseContext} from '../../store/Context'
 function Posts() {
+  const[products,setproducts]=useState([])
+  const {firebase}=useContext(FirebaseContext)
+  useEffect(()=>{
+    firebase.firestore().collection('productss').get().then((snapshot)=>{
+      const allpost=snapshot.docs.map((product)=>{
+             return{
+              ...product.data(),
+              id:product.id
+             }
+      })
+      console.log(allpost);
+      setproducts(allpost)
+    })
+
+  },[])
+
 
   return (
     <div className="postParentDiv">
@@ -13,24 +28,29 @@ function Posts() {
           <span>View more</span>
         </div>
         <div className="cards">
-          <div
-            className="card"
-          >
+
+          {
+            products.map(product=>{
+      
+         return<div className="card">
             <div className="favorite">
               <Heart></Heart>
             </div>
             <div className="image">
-              <img src="../../../Images/R15V3.jpg" alt="" />
+              <img src={product.url} alt="" />
             </div>
             <div className="content">
-              <p className="rate">&#x20B9; 250000</p>
+              <p className="rate">&#x20B9; {product.price}</p>
               <span className="kilometer">Two Wheeler</span>
-              <p className="name"> YAMAHA R15V3</p>
+              <p className="name"> {product.name}</p>
             </div>
             <div className="date">
-              <span>Tue May 04 2021</span>
+              <span>{product.createdat}</span> 
             </div>
           </div>
+            })
+          }
+
         </div>
       </div>
       <div className="recommendations">
